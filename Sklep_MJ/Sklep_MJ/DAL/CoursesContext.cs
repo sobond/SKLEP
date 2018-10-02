a267272885e2c6a646cc22c3fb41700569d33aeb
Daniel Soboń <Daniel.Sobon@bzwbk.pl>
2018-10-02 15:13:35 +0200

    dodanie połączenia do bazy danych

---
+++ Sklep_MJ/Sklep_MJ/DAL/CoursesContext.cs
@@ -0,0 +1,20 @@
+﻿using Sklep_MJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sklep_MJ.DAL
+{
+    public class CoursesContext : DbContext
+    {
+        public CoursesContext() : base("CoursesContext")
+        { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderPosition> GetOrderPositions { get; set; }
+    }
+}

edba323cd2da33af5aff54827e3844385115b3fc
Daniel Soboń <Daniel.Sobon@bzwbk.pl>
2018-10-19 13:32:46 +0200

    dodanie budli dla styli css + update bundla jquery

--- Sklep_MJ/App_Start/BundleConfig.cs
+++ Sklep_MJ/App_Start/BundleConfig.cs
@@ -10,7 +10,13 @@ namespace Sklep_MJ.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~bundles/jquery").Include("~/Scripts/jquery.{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js",
+                                                                    "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include("~/Content/themes/base/core.css",
+                                                                            "~/Content/themes/base/autocomplete.css",
+                                                                            "~/Content/themes/base/theme.css",
+                                                                            "~/Content/themes/base/menu.css"));
         }
     }
 }

a7c6604d27235fa380ebe9a0f7b3fda41ef395e6
Daniel Soboń <Daniel.Sobon@bzwbk.pl>
2018-10-26 13:57:08 +0200

    Dodanie panelu zarządzania kontem uzytkownika

---
+++ Sklep_MJ/ViewModels/ManageCredentialsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Sklep_MJ.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sklep_MJ.ViewModels
+{
+    public class ManageCredentialsViewModel
+    {
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public Sklep_MJ.Controllers.ManageController.ManageMessageId? Message { get; set; }
+        public UserData UserData { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Obecne hasło")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Nowe hasło")]
+        [StringLength(100, MinimumLength =6)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Potwierdź nowe hasło")]
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
